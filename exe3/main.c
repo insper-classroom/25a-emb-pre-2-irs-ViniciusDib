817b78b20562ef761dad13c49cee3772638051f9
Vinicius Dib <viniciusdib15@gmail.com>
2025-02-20 03:10:52 -0500

    teste_ex3

--- exe3/main.c
+++ exe3/main.c
@@ -5,12 +5,15 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile bool estador = false;
+volatile bool estadog = false;
+
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
+  if (events == GPIO_IRQ_EDGE_FALL) { 
     if (gpio == BTN_PIN_R)
-      printf("fall red\n");
+      estador = true;
     else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+      estadog = true;
   }
 }
 
@@ -25,13 +28,21 @@ int main() {
   gpio_set_dir(BTN_PIN_G, GPIO_IN);
   gpio_pull_up(BTN_PIN_G);
 
-  // callback led r (first)
+
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                      &btn_callback);
 
-  // callback led g (nao usar _with_callback)
+
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (estador){
+      printf("fall red \n");
+      estador = false;
+    }
+    if (estadog){
+      printf("fall green \n");
+      estadog = false;
+    }
   }
 }
